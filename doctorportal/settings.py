788ffabc787aa6c13007921426c0c47beeffff4e
bragadeeshwaran <146009886+Jayanthi1237@users.noreply.github.com>
2024-04-30 22:47:57 +0530

    newcommit

--- doctorportal/settings.py
+++ doctorportal/settings.py
@@ -20,15 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECRET_KEY = 'django-insecure-!jnq*k6(*kj8*-6@v@%$04k28&!tgvg+1qqgenzry=_!)14j!!'
-# DEBUG = True
-# ALLOWED_HOSTS = []
+SECRET_KEY = 'django-insecure-!jnq*k6(*kj8*-6@v@%$04k28&!tgvg+1qqgenzry=_!)14j!!'
+DEBUG = True
+ALLOWED_HOSTS = []
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 AUTH_USER_MODEL = 'usercredentials.Profile'
+
 
 
 # Application definition
@@ -86,9 +87,9 @@ DATABASES = {
     }
     
 }
+
 database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
